fd0c37fd13eec2f35a99d9603984bfe11cbd5e8e
grateful-closure-7122 <alex@artofalexwaller.com>
2020-08-20 02:21:32 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,4 +6,13 @@ def turn_count(board)
     end
   end
 return turns
+end
+
+def current_player(board)
+  turns=turn_count(board)
+  if turns % 2 == 0
+    return "X"
+  else
+    return "0"
+  end
 end  
